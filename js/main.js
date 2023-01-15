fa496546c9393fd178222eec149db7589023b73d
Jazlyn Fuller <86030495+JFuller333@users.noreply.github.com>
2023-01-15 11:11:02 -0800

    Add Javascript  Wellness App to portfolio 1.15.23

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var spanElement = document.querySelector(".creative");
+
+spanElement.addEventListener("mouseover", function () {
+spanElement.innerText = "Spontaneity";
+
+});
+
+// H1 span text change when mouse hover over
+
+spanElement.addEventListener("mouseout"), function () {
+  spanElement.innerText =
+}
